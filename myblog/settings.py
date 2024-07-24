a2d48df30e35baa7f25bff914175b438bd8a755c
Lisa <frolovaie9@gmail.com>
2024-07-24 19:14:59 +0100

    base, post list and detail templates are added

--- myblog/settings.py
+++ myblog/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'myblog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,3 +122,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# specify where to collect blog.css files for deployment
+STATICFILES_DIRS = [BASE_DIR / 'static']
